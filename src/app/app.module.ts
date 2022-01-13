7a10fc55495cd754c505cb517694df3ede1d5730
Crymeloun <leon838@web.de>
2022-01-13 11:18:57 +0100

    1 file modified

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,7 +4,7 @@ import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { HelloComponent } from './hello.component';
-import { ShowList } from './components/ShowList.component';
+import { ShowList } from './components/show-list/show-list.component';
 
 @NgModule({
   imports:      [ BrowserModule, FormsModule ],
